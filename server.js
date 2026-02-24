2e97addf488d671f2cdb06e578fdc6791e76bd8b
Ayan <25bcla12@kristujayanti.com>
2026-02-24 11:19:54 +0530

    Re-targeted engine to AyanCluster

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname)));
 
-const mongoURI = 'mongodb+srv://ayan-admin:2025bcla12%40kristujayanti.com@cluster0.c2y9jgu.mongodb.net/ayan-logs?retryWrites=true&w=majority'; 
+const mongoURI = 'mongodb+srv://ayan-admin:2025bcla12%40kristujayanti.com@ayancluster.xxxx.mongodb.net/ayan-logs?retryWrites=true&w=majority'; 
 
 mongoose.connect(mongoURI)
     .then(() => console.log("Connected to MongoDB! Ayan's Vault is secure."))
